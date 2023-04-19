963ecbc98234bedd651bbda631d5c676345adcd5
Oronemu <Oronemu@mail.ru>
2023-04-19 19:19:10 +0700

    Initial commit

---
+++ ADMP Pro/ADMP_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADMP_ProApp.swift
+//  ADMP Pro
+//
+//  Created by Иван Ровков on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ADMP_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADMP Pro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ADMP Pro
+//
+//  Created by Иван Ровков on 19.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
